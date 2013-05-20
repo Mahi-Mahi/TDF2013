44cdf2ac674555851e7daef5f11e21dbb4599e7c
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2013-05-20 20:25:14 +0200

    create winners.json, tours.json // fetch all vainqueurs.csv

--- data/convert.rb
+++ data/convert.rb
@@ -1,18 +1,116 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 if RUBY_VERSION =~ /1.9/
   Encoding.default_external = Encoding::UTF_8
   Encoding.default_internal = Encoding::UTF_8
 end
 
+
 require 'csv'
 require 'json'
 require 'pp'
 require 'zlib'
+require 'fileutils'
+
+production = false
+
+
+FileUtils.rm_rf("json/winners/.", secure: true)
+FileUtils.rm_rf("json/tours/.", secure: true)
+FileUtils.rm Dir.glob('json/*.json')
+FileUtils.rm Dir.glob('json/*.gz')
+
 
 legs = []
 short_legs = []
 short_legs_fields = ['id', 'year', 'leg', 'title', 'start', 'finish']
+tours = {}
+winners = {}
+
+idx = 0
+CSV.foreach("csv/Données - Tours - Feuille1.csv") do |row|
+
+	if idx > 0
+#		p row
+
+		tour = {}
+		tour[:year] = row[0].to_i
+
+		tour[:edition] = row[1].to_i
+
+		tour[:total_length] = row[2].to_i
+		tour[:nb_legs] = row[3].to_i
+		tour[:has_prologue] = (row[4] == 'o')
+
+		tour[:nb_concurrents] = row[5].to_i
+		tour[:nb_finishers] = row[6].to_i
+		tour[:pct_finishers] = row[7].to_i
+
+		tour[:winner_name] = row[8]
+		tour[:winner_time] = row[9]
+
+		tour[:ahead_of_2nd] = row[10]
+		tour[:second_name] = row[11]
+		tour[:second_country] = row[12]
+
+		tour[:ahead_of_3rd] = row[13]
+		tour[:third_name] = row[14]
+		tour[:third_country] = row[14]
+
+		tour[:legs] = []
+
+		tours[tour[:year]] = tour
+
+	end
+
+	idx = idx + 1
+
+end
+
+#puts tours
+puts "#{tours.length} tours"
+
+
+
+idx = 0
+CSV.foreach("csv/Données - Vainqueurs - Feuille1.csv") do |row|
+
+	if idx > 0
+#		p row
+
+		id = row[0]
+		year = row[5].to_i
+
+		if winners[id].nil?
+			winners[id] = {}
+			winners[id][:id] = id
+			winners[id][:first_name] = row[1]
+			winners[id][:last_name] = row[2]
+			winners[id][:birthyear] = row[5].to_i - row[3].to_i
+			winners[id][:country] = row[4]
+			winners[id][:years] = {}
+		end
+
+		winners[id][:years][year] = {}
+		winners[id][:years][year][:nb_days_in_yellow] = row[6].to_i
+		winners[id][:years][year][:nb_tour_legs] = row[7].to_i
+		winners[id][:years][year][:pct_leading] = row[8].to_i
+		winners[id][:years][year][:nb_leg_wins] = row[9].to_i
+		winners[id][:years][year][:average_speed] = row[10].to_i
+		winners[id][:years][year][:total_time] = row[11]
+		winners[id][:years][year][:ahead_of_2nd] = row[12]
+		winners[id][:is_doped] = ( row[14] == 'oui' )
+
+		winners[id][:nb_wins] = row[15]
+
+	end
+
+	idx = idx + 1
+
+end
+
+puts "#{winners.length} winner"
 
 
 idx = 0
@@ -24,8 +122,8 @@ CSV.foreach("csv/Etapes_Tour_Total_Geoloc - Feuille1.csv") do |row|
 
 		leg[:id] = row[0].gsub(/\-(\d)$/, '-0\1')
 
-		leg[:year] = row[1]
-		leg[:leg] = row[2]
+		leg[:year] = row[1].to_i
+		leg[:leg] = row[2].to_i
 		leg[:title] = row[3]
 
 		leg[:start] = {}
@@ -43,7 +141,7 @@ CSV.foreach("csv/Etapes_Tour_Total_Geoloc - Feuille1.csv") do |row|
 		leg[:finish][:lng] = row[13]
 		leg[:finish][:lat] = row[14]
 
-		leg[:length] = row[15]
+		leg[:length] = row[15].to_i
 
 #		leg[:finish][:coords_inv] = row[16]
 
@@ -57,20 +155,72 @@ CSV.foreach("csv/Etapes_Tour_Total_Geoloc - Feuille1.csv") do |row|
 		leg[:leader][:name] = row[20]
 		leg[:leader][:nationality] = row[21]
 
+		unless tours[leg[:year]].nil?
+
+			tours[leg[:year]][:legs] << leg
+
+		end
+
 		legs << leg
 
 		short_legs << leg.clone.keep_if {|k,v| short_legs_fields.include? k }
+
+		# break	if idx > 30
 
 	end
 
 	idx = idx + 1
 
 end
-
 puts "#{legs.length} etapes"
 
-File.open('json/legs.json', 'w') { |file| file.write legs.to_json }
-Zlib::GzipWriter.open('json/legs.json.gz') { |file| file.write legs.to_json }
+# puts tours.first
+# puts tours.first[1][:legs]
 
-File.open('json/legs-short.json', 'w') { |file| file.write short_legs.to_json }
-Zlib::GzipWriter.open('json/legs-short.json.gz') { |file| file.write short_legs.to_json }
+
+tours.each do |year, tour|
+	tour[:legs].sort_by! {|o| o[:leg] }
+end
+
+# Loops
+
+filename = "json/tours.json"
+content = production ? tours.to_json : JSON.pretty_generate(tours)
+File.open(filename, 'w') { |file| file.write content }
+Zlib::GzipWriter.open("#{filename}.gz") { |file| file.write content }
+
+tours.each do |year, tour|
+	filename = "json/tours/#{year}.json"
+	content = production ? tour.to_json : JSON.pretty_generate(tour)
+	File.open(filename, 'w') { |file| file.write content }
+	Zlib::GzipWriter.open("#{filename}.gz") { |file| file.write content }
+end
+
+# Winners
+# one file by tour
+
+filename = "json/winners.json"
+content = production ? winners.to_json : JSON.pretty_generate(winners)
+File.open(filename, 'w') { |file| file.write content }
+Zlib::GzipWriter.open("#{filename}.gz") { |file| file.write content }
+
+winners.each do |id, winner|
+	filename = "json/winners/#{id}.json"
+	content = production ? winner.to_json : JSON.pretty_generate(winner)
+	File.open(filename, 'w') { |file| file.write content }
+	Zlib::GzipWriter.open("#{filename}.gz") { |file| file.write content }
+end
+
+
+# Test all legs
+
+filename = 'json/legs.json'
+content = production ? legs.to_json : JSON.pretty_generate(legs)
+File.open(filename, 'w') { |file| file.write content }
+Zlib::GzipWriter.open("#{filename}.gz") { |file| file.write content }
+
+filename = 'json/legs-short.json'
+content = production ? short_legs.to_json : JSON.pretty_generate(short_legs)
+File.open(filename, 'w') { |file| file.write content }
+Zlib::GzipWriter.open("#{filename}.gz") { |file| file.write content }
+
---
+++ data/fetch_csv.rb
@@ -0,0 +1,100 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'rubygems'
+require 'google/api_client'
+require 'launchy'
+require 'pp'
+
+
+## Email of the Service Account #
+SERVICE_ACCOUNT_EMAIL = '946494525746-m6epcp5cqg7i09deg3ogebruicah1uhq@developer.gserviceaccount.com'
+
+## Path to the Service Account's Private Key file #
+SERVICE_ACCOUNT_PKCS12_FILE_PATH = 'gdrive-secret/28c68e93965f567d839a31f630057df2482b5ffd-privatekey.p12'
+
+
+client = Google::APIClient.new
+drive = client.discovered_api('drive', 'v2')
+
+
+auth_type = 'standard'
+
+if auth_type == 'service_account'
+
+	key = Google::APIClient::PKCS12.load_key(SERVICE_ACCOUNT_PKCS12_FILE_PATH, 'notasecret')
+	asserter = Google::APIClient::JWTAsserter.new(SERVICE_ACCOUNT_EMAIL, 'https://www.googleapis.com/auth/drive', key)
+	client.authorization = asserter.authorize()
+
+end
+
+if auth_type == 'standard'
+
+	# Get your credentials from the APIs Console
+	CLIENT_ID = '946494525746.apps.googleusercontent.com'
+	CLIENT_SECRET = 'SDLKPeKexiEvrWJm1wdlRfaA'
+	OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+	REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+	# Create a new API client & load the Google Drive API
+
+	# Request authorization
+	client.authorization.client_id = CLIENT_ID
+	client.authorization.client_secret = CLIENT_SECRET
+	client.authorization.scope = OAUTH_SCOPE
+	client.authorization.redirect_uri = REDIRECT_URI
+
+	uri = client.authorization.authorization_uri
+	Launchy.open(uri)
+
+	# Exchange authorization code for access token
+	$stdout.write  "Enter authorization code: "
+	client.authorization.code = gets.chomp
+	client.authorization.fetch_access_token!
+
+end
+
+
+folder_id = "0B-mnANvdRfRjVG44eG9aQ1RXdUk"
+
+page_token = nil
+begin
+	parameters = {'folderId' => folder_id, 'maxResults' => 60}
+	if page_token.to_s != ''
+		parameters['pageToken'] = page_token
+	end
+	folder_result = client.execute(:api_method => drive.children.list, :parameters => parameters)
+	if folder_result.status == 200
+		children = folder_result.data
+		children.items.each do |child|
+
+			puts "File Id: #{child.id}"
+
+			result = client.execute( :api_method => drive.files.get, :parameters => { 'fileId' => child.id })
+			if result.status == 200
+				file = result.data
+				puts "#{file.title}"
+				download_link = file['exportLinks']['application/pdf'].gsub!(/exportFormat=\w+$/, 'exportFormat=csv&gid=0')
+
+				dl_result = client.execute(:uri => download_link)
+				if dl_result.status == 200
+					content = dl_result.body
+
+					filename = "csv/vainqueurs/#{file.title}.csv"
+					File.open(filename, 'w') { |file| file.write content }
+
+				else
+					puts "An error occurred: #{result.data['error']['message']}"
+				end
+
+			else
+				puts "An error occurred: #{result.data['error']['message']}"
+			end
+
+		end
+		page_token = children.next_page_token
+	else
+		puts "An error occurred: #{folder_result.data['error']['message']}"
+		page_token = nil
+	end
+end while page_token.to_s != ''
