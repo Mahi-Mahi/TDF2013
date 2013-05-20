b371d55105ba15cf1475301c8a9811aecd34b039
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2013-05-20 16:07:04 +0200

    correctin inversion lat/lng

--- data/convert.rb
+++ data/convert.rb
@@ -33,24 +33,28 @@ CSV.foreach("csv/Etapes_Tour_Total_Geoloc - Feuille1.csv") do |row|
 #		leg[:start][:coords] = row[6]
 		leg[:start][:lng] = row[7]
 		leg[:start][:lat] = row[8]
+#		leg[:start][:coords_inv] = row[9]
 
 		leg[:finish] = {}
-		leg[:finish][:city] = row[9]
-		leg[:finish][:country] = row[10]
-#		leg[:finish][:coords] = row[11]
-		leg[:finish][:lng] = row[12]
-		leg[:finish][:lat] = row[13]
+		leg[:finish][:city] = row[10]
+		leg[:finish][:country] = row[11]
+#		leg[:finish][:coords] = row[12]
+		leg[:finish][:lng] = row[13]
+		leg[:finish][:lat] = row[14]
 
-		leg[:length] = row[14]
-		leg[:type] = row[15]
+		leg[:length] = row[15]
+
+#		leg[:finish][:coords_inv] = row[16]
+
+		leg[:type] = row[17]
 
 		leg[:winner] = {}
-		leg[:winner][:name] = row[16]
-		leg[:winner][:nationality] = row[17]
+		leg[:winner][:name] = row[18]
+		leg[:winner][:nationality] = row[19]
 
 		leg[:leader] = {}
-		leg[:leader][:name] = row[18]
-		leg[:leader][:nationality] = row[19]
+		leg[:leader][:name] = row[20]
+		leg[:leader][:nationality] = row[21]
 
 		legs << leg
 
