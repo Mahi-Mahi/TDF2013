a512617f22437b456eafdcc5b332daa2d02a1766
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2013-05-29 15:43:25 +0200

    bower/grunt setup

---
+++ web/src/js/main.js
@@ -0,0 +1,18 @@
+( function () {
+
+  var Revue = window.Revue || {};
+
+  Revue.init = function () {
+
+
+  };
+
+  // Document Ready
+  jQuery( document ).ready( function () {
+
+    Revue.init();
+
+  } );
+
+
+} )();
