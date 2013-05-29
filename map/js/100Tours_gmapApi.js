ca79145c2875a6909de95db7a6962f2dcc4b0af5
Alexandre Peyron <alexandre.peyron@gotoandbuzz.com>
2013-05-29 10:23:01 +0200

    start geoloc

---
+++ map/js/100Tours_gmapApi.js
@@ -0,0 +1,108 @@
+var gmap,
+    map,
+    mapId,
+    mapTypeId,
+    startlat,
+    startlng,
+    allEtapes,
+    toursSelecteur;
+
+
+$(document).ready(function(){
+
+        //Config Gmap
+        gmap = null;
+        mapId = 'mapPreview';
+        mapTypeId = google.maps.MapTypeId.ROADMAP;
+        startlat = 47.754098;
+        startlng = 3.669434;
+
+        //Data
+        allEtapes = null;
+
+        //Elements
+        map = $("#" + mapId);
+        toursSelecteur = $('#toursSelecteur');
+
+
+
+        google.maps.event.addDomListener(window, 'load', initializeGmap);
+        
+        loadData();
+
+});
+
+
+function initializeGmap() {
+       
+        var mapOptions = {
+            mapTypeId: mapTypeId,
+            center: new google.maps.LatLng(startlat, startlng),
+            zoom: 6,
+            zoomControl : true,
+            zoomControlOpt: {
+                style : 'SMALL',
+                position: 'TOP_LEFT'
+            }
+        };
+
+        gmap = map.gmapApi(mapOptions);
+}
+
+
+function loadData(){
+    
+    
+    $.getJSON('data/data-all.json', function(data) {
+
+        allEtapes = data;
+
+        var currentYear = 0;
+        var options = [];
+
+        $.each(data, function(num, etape) {
+
+            if(currentYear != etape.year){
+                currentYear = etape.year;
+
+                options.push('<option value="' + etape.year + '">' + etape.year + '</option>');
+
+                toursSelecteur.html(options.join(''));
+            }    
+        });
+
+        initSelecteur();
+    });
+}
+
+
+function initSelecteur(){
+
+    console.log("allEtapes.length : " + allEtapes.length);
+
+        toursSelecteur.on('change', function(){
+            var years = $(this).val();
+
+            console.log("changeSelecteur: " + years);
+
+            gmap.createEtapes(years, allEtapes);
+
+        });
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ map/js/home_gmapApi.js
@@ -0,0 +1,88 @@
+var gmap,
+    map,
+    mapId,
+    mapTypeId,
+    startlat,
+    startlng,
+    allEtapes,
+    searchInput;
+
+
+$(document).ready(function(){
+
+        //Config Gmap
+        gmap = null;
+        mapId = 'mapPreview';
+        mapTypeId = google.maps.MapTypeId.ROADMAP;
+        startlat = 47.754098;
+        startlng = 3.669434;
+
+        //Data
+        allEtapes = null;
+
+        //Elements
+        map = $("#" + mapId);
+
+        searchInput = $('#inputGeoloc');
+
+
+        google.maps.event.addDomListener(window, 'load', initializeGmap);
+        
+        loadData();
+
+});
+
+
+function initializeGmap() {
+       
+        var mapOptions = {
+            mapTypeId: mapTypeId,
+            center: new google.maps.LatLng(startlat, startlng),
+            zoom: 6,
+            zoomControl : true,
+            zoomControlOpt: {
+                style : 'SMALL',
+                position: 'TOP_LEFT'
+            }
+        };
+
+        gmap = map.gmapApi(mapOptions);
+}
+
+
+function loadData(){
+    
+    
+    $.getJSON('data/data-all.json', function(data) {
+
+        allEtapes = data;
+
+        var currentYear = 0;
+        var options = [];
+
+        $.each(data, function(num, etape) {
+
+            if(currentYear != etape.year){
+                currentYear = etape.year;
+
+            }    
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
