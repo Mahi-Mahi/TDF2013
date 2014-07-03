c363b3385ddf75a1ae3476fd83c73a530e92330e
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-07-03 12:57:25 +0200

    fixes

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -484,7 +484,7 @@ TDF.Home = (function() {
 	my.name = 'home';
 
 	my.base_url = '/';
-	my.share_text = "Explorez les #données des 100 Tours de France : tracés, vainqueurs, lieux mythiques... #appli #data #TDF via @RFnvx";
+	my.share_text = "Explorez les #données des 101 Tours de France : tracés, vainqueurs, lieux mythiques... #appli #data #TDF via @RFnvx";
 
 	my.init = function() {
 		// Set handler ( only on first init )
@@ -595,7 +595,7 @@ TDF.CitySearch = (function() {
 
 	my.name = 'search';
 
-	my.share_text = "Explorez les #données des 100 Tours de France : tracés, vainqueurs, lieux mythiques... #appli #data #TDF via @RFnvx";
+	my.share_text = "Explorez les #données des 101 Tours de France : tracés, vainqueurs, lieux mythiques... #appli #data #TDF via @RFnvx";
 	my.share_hashtags = "appli,data,TDF";
 
 	my.gmapApi = null;
@@ -768,7 +768,7 @@ TDF.Traces = (function() {
 	my.data = null;
 	my.args = null;
 
-	my.share_text = "Découvrez les tracés des 100 éditions du Tour de France dans une #carte interactive #appli #data #TDF via @RFnvx";
+	my.share_text = "Découvrez les tracés des 101 éditions du Tour de France dans une #carte interactive #appli #data #TDF via @RFnvx";
 
 	my.city_years = [];
 	my.city_slider_set = false;
@@ -825,7 +825,7 @@ TDF.Traces = (function() {
 					Path.history.pushState({}, "", my.base_url + years.join(',') + '/');
 
 				} else {
-					Path.history.pushState({}, "", my.base_url + (my.last_clicked ? my.last_clicked : '2013') + '/');
+					Path.history.pushState({}, "", my.base_url + (my.last_clicked ? my.last_clicked : '2014') + '/');
 				}
 			}
 		});
@@ -833,7 +833,7 @@ TDF.Traces = (function() {
 		$main.on('click', '.traces #multi-select', function() {
 			if (my.state === null && !my.state_interval) {
 				if (!my.last_clicked) {
-					my.last_clicked = 2013;
+					my.last_clicked = 2014;
 				}
 				if (jQuery(this).prop('checked')) {
 					jQuery('.traces #start-pause').addClass('opaque');
@@ -899,7 +899,7 @@ TDF.Traces = (function() {
 					jQuery('.traces #multi-select').prop('disabled', 'disabled');
 					jQuery('.traces #select-all').prop('disabled', 'disabled');
 					jQuery('.traces #start-pause').addClass('active');
-					if (jQuery('.timeline-zoom input:checked').val() === '2013') {
+					if (jQuery('.timeline-zoom input:checked').val() === '2014') {
 						jQuery('.timeline-zoom li:first span').click();
 					} else {
 						// jQuery('.timeline-zoom input:checked').parent().next().find('span').click();
@@ -1079,7 +1079,7 @@ TDF.Traces = (function() {
 
 		if (my.args.years === undefined) {
 			if (my.args.city === undefined) {
-				my.args.years = [2013];
+				my.args.years = [2014];
 				my.city_years = [];
 				$main.find('#search').val('');
 			} else {
@@ -1308,13 +1308,13 @@ TDF.Traces = (function() {
 			width: Math.round((nb_concurrents - my.stats['nb_concurrents'].min.val) / (my.stats['nb_concurrents'].max.val - my.stats['nb_concurrents'].min.val) * line_length) + 'px'
 		});
 
-		$main.find(".nb_finishers .current").html((my.args.years.length === 0 || my.args.years[0] === 2013 || my.args.years[0] === '2013') ? 'N.C.' : nb_finishers + " à l'arrivée");
+		$main.find(".nb_finishers .current").html((my.args.years.length === 0 || my.args.years[0] === 2014 || my.args.years[0] === '2014') ? 'N.C.' : nb_finishers + " à l'arrivée");
 		$main.find(".nb_finishers .line").css({
 			width: Math.round((nb_finishers - my.stats['nb_finishers'].min.val) / (my.stats['nb_finishers'].max.val - my.stats['nb_finishers'].min.val) * line_length) + 'px'
 		});
 
 		// Winners
-		if (my.args.years.length === 1 && parseInt(my.args.years[0], 10) !== 2013) {
+		if (my.args.years.length === 1 && parseInt(my.args.years[0], 10) !== 2014) {
 			$main.find('.traces-right').removeClass('disabled');
 
 			trace = TDF.Data.traces[my.args.years[0]];
